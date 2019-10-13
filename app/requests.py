09a9c04d01ee9c6051b82b970e8857589724a28d
brian23-eng <b.odhiambo.bo@gmail.com>
2019-10-13 13:41:23 +0300

    request for the api key  and the base url and the article url, then import the blueprint

--- app/requests.py
+++ app/requests.py
@@ -1,16 +1,22 @@
-from app import app
 import urllib.request,json
-from .models import sources, articles
+from .models import Sources, Articles
 #getting api_key
-Sources = sources.Sources
-articles = articles.Articles
+# Sources = sources.Sources
+# articles = articles.Articles
 
-api_key = app.config['NEWS_API_KEY']
+#getting the api key
+api_key = None
 
 #getting the sources and the articles base api url
 
-base_url = ["NEWS_SOURCES_BASE_URL"]
-articles_base_url =["ARTICLES_BASE_URL"]
+base_url = None
+articles_base_url = None
+
+def configure_requests(app):
+    global api_key,base_url,articles_url
+	api_key = app.config['NEWS_API_KEY']
+	base_url = app.config['NEWS_SOURCES_BASE_URL']
+	articles_url = app.config['ARTICLES_BASE_URL']
 
 def get_sources(category):
 	'''
