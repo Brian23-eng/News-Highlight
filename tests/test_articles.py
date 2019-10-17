19c8816cbcf2980a381d01788ba604cc85c8ebaa
brian23-eng <b.odhiambo.bo@gmail.com>
2019-10-17 11:09:19 +0300

    run unittest to test instances of app in development

--- tests/test_articles.py
+++ tests/test_articles.py
@@ -4,13 +4,9 @@ from app import create_app
 
 class BasicsTestCase(unittest.TestCase):
     def setUp(self):
+        
+        '''
+        Set up method that will run before every Test
+        '''
+        
         self.app = create_app('development')
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-
-    def tearDown(self):
-
-        self.app_context.pop()
-
-    def test_app_exists(self):
-        self.assertFalse(current_app is None)
